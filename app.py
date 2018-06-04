10c78c4236678987062659aa15744eb22b2d28f2
yoeri <yoeri90@gmail.com>
2018-06-04 11:33:32 +0200

    some json output ready to be presented to the user.

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/apps')
+def index():
+    list_of_apps = [{
+                'id': 0,
+                'name': 'test',
+                'size': 3
+            } , {
+                'id': 1,
+                'name': 'booyah',
+                'size': 5
+            }]
+
+    return jsonify(list_of_apps)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
