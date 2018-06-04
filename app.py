4d80b74053ee596ee71b6354001b76e87d2d229d
yoeri <yoeri90@gmail.com>
2018-06-04 12:16:36 +0200

    start of database implementation

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 from flask import Flask, jsonify
+from flask_restplus import Api, Resource, fields
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return jsonify({'status': 'ok'})
+@api.route('/status')
+class Status(Resource):
+    def get(self):
+        return {'status': 'ok'}
 
+@api.route('/apps')
+class Apps(Resource):
+    def get(self):
+        list_of_apps = [{
+                    'id': 0,
+                    'name': 'test',
+                    'size': 3
+                } , {
+                    'id': 1,
+                    'name': 'booyah',
+                    'size': 5
+                }]
+        return list_of_apps
 
-@app.route('/apps')
-def index():
-    list_of_apps = [{
-                'id': 0,
-                'name': 'test',
-                'size': 3
-            } , {
-                'id': 1,
-                'name': 'booyah',
-                'size': 5
-            }]
+@api.route('/app/<app_id>')
+class AppsStatus(Resource):
+    def get(self, app_id):
+        return jsonify({'status': 'ok', 'app_id': app_id})
 
-    return jsonify(list_of_apps)
+    def put(self, app_id):
+        return({'app_id': app_id, 'registered': True})
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
