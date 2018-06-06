f5ee560a79a07b814f9fa9984d9f7883d425468d
yoeri <yoeri90@gmail.com>
2018-06-06 15:44:17 +0200

    now with all files

--- app.py
+++
@@ -1,5 +0,0 @@
-from app import app, db, api
-
-@app.shell_context_processor:
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
